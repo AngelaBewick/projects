ac86d594abc5c326873d3f10602b944261b4fff2
AngelaBewick <angelagracebewick@gmail.com>
2022-07-11 11:27:11 -0500

    delete from DB added

--- public/bobsToDoList/views/deleteView.js
+++ public/bobsToDoList/views/deleteView.js
@@ -1,7 +1,11 @@
-export const deleteBTN = function (e) {
+export const deleteBTN = async function (e) {
   if (e.target.dataset.name === "remove") {
     const btn = e.target;
     const parent = btn.closest(".row");
+    const id = parent.getAttribute("id");
     parent.remove();
+    await fetch(`https://angela-bewicks-api.onrender.com/api/v1/${id}`, {
+      method: "DELETE",
+    });
   }
 };
